b14d6b14ac9a3892374428ce7a36ac11a01b5ad3
wangshengwen <wswen1220@163.com>
2017-04-17 17:42:18 +0800

    webpack-project

---
+++ myProject2/webpack.config.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Administrator on 2017/4/17.
+ */
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: ['./src/index'],
+    output: {
+        path: path.join(__dirname,'dist'),
+        filename:'bundle.js'
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warning: false
+            },
+        }),
+        new webpack.optimize.OccurenceOrderPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loaders: ['style','css']
+        }]
+    }
+
+}
